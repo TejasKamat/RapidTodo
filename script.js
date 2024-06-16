d3c6364b0ca8cd3cab8384e0616191d857e63c99
Tejas Kamat <kamattejas23@gmail.com>
2024-06-16 13:13:22 +0530

    TODO: Initial commit
    Signed-off-by: Tejas Kamat <kamattejas23@gmail.com>

---
+++ script.js
@@ -0,0 +1,32 @@
+const inputbox = document.getElementById("inputbox")
+const listcontainer = document.getElementById("listcontainer")
+const example = document.getElementsByClassName("ex")
+
+const addTask = (value) => {
+    if (inputbox.value === '') {
+        alert("Please enter something")
+    } else {
+
+        for (let i = 0; i < example.length; i++) {
+            example[i].setAttribute("hidden", true)
+        }
+
+        let li = document.createElement("li")
+        li.innerHTML = inputbox.value
+        listcontainer.appendChild(li)
+
+        let span = document.createElement("span")
+        span.innerHTML = "&#x2715;"
+        li.appendChild(span)
+        inputbox.value = ""
+    }
+}
+
+listcontainer.addEventListener("click", (event) => {
+    if (event.target.tagName == "LI") {
+        event.target.classList.toggle("checked")
+    }
+    if (event.target.tagName == "SPAN") {
+        event.target.parentElement.remove("LI")
+    }
+})
